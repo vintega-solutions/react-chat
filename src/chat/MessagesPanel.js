21a0b90d15548b07c31158ccb70c4e2f4d3593e0
asadahmedtahir <76432848+asadahmedtahir@users.noreply.github.com>
2021-04-01 19:49:00 +0500

    Update MessagesPanel.js

--- src/chat/MessagesPanel.js
+++ src/chat/MessagesPanel.js
@@ -4,7 +4,7 @@ import { Message } from './Message';
 export class MessagesPanel extends React.Component {
     state = { input_value: '' }
     send = () => {
-        if (this.state.input_value && this.state.input_value != '') {
+        if (this.state.input_value && this.state.input_value !== '') {
             this.props.onSendMessage(this.props.channel.id, this.state.input_value);
             this.setState({ input_value: '' });
         }
